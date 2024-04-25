421a3d7f496d5ed854bb6c395dc5ffd2c05689c9
Andrew Bollard <bollard92@gmail.com>
2024-04-24 23:54:20 -0500

    rough draft

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # 5dayweather
 5 day weather tracker
 
-submitted progress. will complete and re submit.
+This five day wether tracker will allow you to search by city the weather for the next five days. The following will be provided each day for the selected city, 
+
+https://drive.google.com/file/d/1sznIeCEcq_bZBLSJLallzCQs4Cir63iY/view?usp=sharing
--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const city = $('#city')
 const button = $('.action')
 const form = $('.form') 
 
+let cityNames = JSON.parse(localStorage.getItem("cityNames")) || [];
+
+
 
 function locationPull() {
     const locationApi = `https://api.openweathermap.org/geo/1.0/direct?q=${city.val()}&appid=${apiKey}`
@@ -14,21 +17,37 @@ function locationPull() {
             console.log(data[0].lon) 
             const lat = data[0].lat
             const lon = data[0].lon
-
             weatherPull(lat , lon)
+            cityNames.push(data[0].name);
+            localStorage.setItem("cityNames", JSON.stringify(cityNames));
+            removeDoubles()
             }
-        );       
+        ); 
 }
+
+function removeDoubles() {
+    cityNames = cityNames.filter( (name, i) => !cityNames.includes(name, i+1))
+    localStorage.setItem("cityNames" , JSON.stringify(cityNames))
+    console.log(cityNames)
+};
+
+function buttonMaker() {
+    console.log("buttonmaker", cityNames)
+    for (let i = 0; i < cityNames.length; i++) {
+    let button = $("<button>").text(cityNames[i]).attr("id", cityNames[i]).addClass("hButtons").appendTo($(".history"));
+    
+}
+};
 
 function weatherPull(lat , lon) {
     console.log(lat, lon)
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`) 
-        .then(response => response.json())
-        .then(data => {
-            console.log(data) 
-            saveToStorage(data)          
+    .then(response => response.json())
+    .then(data => {
+        console.log(data) 
+        saveToStorage(data)          
         dataPopulate();    }
-        );
+    );
     
 }
 
@@ -46,23 +65,22 @@ function saveToStorage(cityWeather) {
 
 function dataPopulate() {
     const weather = readLocalStorage()
-    const today = $("<div>").appendTo($(".today"));
+    const cityName = $("<h1>").text(city.val()).appendTo($(".today"));
+    const today = $("<div>").addClass("superDuper").appendTo($(".today"));
     const date = $("<p>").text(dayjs.unix(weather.list[0].dt).format("MM/DD/YYYY")).appendTo(today);
     const temp = $("<p>").text(`Temp:${weather.list[0].main.temp}`).appendTo(today);
     const wind = $("<p>").text(`Wind:${weather.list[0].wind.speed}/mph`).appendTo(today);
     const humidity = $("<p>").text(`Humidity:${weather.list[0].main.humidity}%`).appendTo(today);
     
     for (let i = 1; i < weather.list.length; i+=7) {
-        const forcast = $("<div>").appendTo($(".forcast"));
+        const forcast = $("<div>").addClass("superDuper").appendTo($(".forcast"));
         const date = $("<p>").text(dayjs.unix(weather.list[i].dt+86400).format("MM/DD/YYYY")).appendTo(forcast);
         const temp = $("<p>").text(`Temp:${weather.list[i].main.temp}`).appendTo(forcast);
         const wind = $("<p>").text(`Wind:${weather.list[i].wind.speed}/mph`).appendTo(forcast);
         const humidity = $("<p>").text(`Humidity:${weather.list[i].main.humidity}%`).appendTo(forcast);
-
+        
     }
 };
-
-//need to link button to run weather pull
 
 
 
@@ -72,13 +90,34 @@ form.on("click", ".action", function(e){
     e.preventDefault()
     locationPull();
     console.log(city.val())
-    //saveToStorage()
+    $('.today').empty()
+    $('.forcast').empty()
+    
 });
 
+buttonMaker();
 
 // local storage holding most recent searches
 
 
+
+$(".history").on("click", ".hButtons", function(e){
+    e.preventDefault()
+    const cityName = $(this).attr("id");
+    $('.today').empty()
+    $('.forcast').empty()
+    const locationApi = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
+    const clickedCityName = $("<h1>").text(cityName).appendTo($(".today"));
+    fetch(locationApi)
+    .then(response => {
+        return response.json()
+    }). then(function(data){
+        const lat = data[0].lat
+        const lon = data[0].lon
+        localStorage.setItem("city", JSON.stringify(data))
+        weatherPull(lat , lon)
+    })
+})
 
 
 
